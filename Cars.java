1a08ce80044d4f3580537f7b89286d5f5f55ab4e
Sirigidi Abhishek <119723474+Abhisheksirigidi@users.noreply.github.com>
2023-08-25 09:46:27 +0530

    import java.util.Scanner; public class main{     public static void main(String[] args){         Scanner s = new Scanner(System.in);         int n1=s.nextInt(),n2=s.nextInt(),x=s.nextInt();         int sa;         if(n1>=n2){             System.out.println(-1);         }         else{             sa=(x/(n2-n1))+1;             System.out.println(sa);         }     } }

---
+++ Cars.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class main{
+    public static void main(String[] args){
+        Scanner s = new Scanner(System.in);
+        int n1=s.nextInt(),n2=s.nextInt(),x=s.nextInt();
+        int sa;
+        if(n1>=n2){
+            System.out.println(-1);
+        }
+        else{
+            sa=(x/(n2-n1))+1;
+            System.out.println(sa);
+        }
+    }
+}
